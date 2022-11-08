7b02756ec37f4501bdd02a0cee743272479f12af
Timothee Laumonier <99754210+iXwin@users.noreply.github.com>
2022-11-08 10:57:37 +0000

    Debut

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+char aleaCharactere(){
+    int i;
+    char alea;
+    alea = (rand() % 26) + 97 ;
+    return alea;
+}
+int verif(a){
+  
+}
+
+void initialisation(){
+    srand(time(NULL));
+    int i=0,j=0;
+    char tab[10][20];
+  
+    for (int i = 0; i < 10 ; ++i) {
+        for (int j = 0; j < 20 ; ++j) {
+          tab[i][j] =aleaCharactere();
+          printf("%c",tab[i][j]);
+        }
+        printf("\n");
+    }
+    
+}
+
+int main(){
+    initialisation();
 }
